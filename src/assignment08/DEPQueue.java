1756bf8b45fd2ce3d61148fc6dd38c8bd8bef9a4
Boden-Archuleta <boden.archuleta@gmail.com>
2017-11-27 16:39:12 -0700

    Initial Commit

---
+++ src/assignment08/DEPQueue.java
@@ -0,0 +1,36 @@
+package assignment08;
+
+public class DEPQueue {
+	DEPQueue(){
+		
+	}
+	
+	public  void  insert (String data){
+		
+	}
+	
+	public String removeMin(){
+		
+		return null;
+	}
+	
+	public String removeMax(){
+		
+		return null;
+	}
+	
+	public int size(){
+		
+		return 0;
+	}
+	
+	public long getComparisonCount (){
+		
+		return 0;
+	}
+	
+	public long getSwapCount(){
+		
+		return 0;
+	}
+}
